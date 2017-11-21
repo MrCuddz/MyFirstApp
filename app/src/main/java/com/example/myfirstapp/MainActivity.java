e470a42ec18b802afb0cfd9de84959f3dbda31db
MrCuddz <mc-cuddihy@hotmail.com>
2017-11-21 13:54:14 +0000

    Added Slide out drawer navigation menu

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,20 +1,71 @@
 package com.example.myfirstapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private DrawerLayout mDrawer;
+    private ListView mDrawerListView;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private TextView messageTextView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        messageTextView = (TextView) findViewById(R.id.messageTextView);
+        mDrawer= (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // These lines are needed to display the top-left hamburger button
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //make ha,burger button work
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawer, R.string.app_name,R.string.app_name){
+          @Override
+            public void onDrawerClosed(View drawerView){
+
+          }
+          @Override
+            public void onDrawerOpened(View drawerView){
+
+          }
+        };
+        mDrawer.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
+        // Change Activity when ListView item is clicked
+        mDrawerListView = (ListView) findViewById(R.id.left_drawer);
+        mDrawerListView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //TODO create activity on click
+                /*// Start NewActivity.class
+				Intent myIntent = new Intent(MainActivity.this,
+						NewActivity.class);
+				startActivity(myIntent);*/
+
+                messageTextView.setText("Menu Item at position " + position + " clicked.");;
+                mDrawer.closeDrawer(GravityCompat.START);
+            }
+        });
     }
 
     /** Called when the user taps the Send button */
@@ -27,4 +78,29 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 }
+
